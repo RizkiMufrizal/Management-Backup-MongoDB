f620569cadc171b7f8de0671872768f1a14a08ed
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-11 20:39:22 +0700

    initial project

---
+++ src/main/java/com/rizki/mufrizal/management/backup/mongodb/ManagementBackupMongoDbApplication.java
@@ -0,0 +1,12 @@
+package com.rizki.mufrizal.management.backup.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementBackupMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementBackupMongoDbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rizki/mufrizal/management/backup/mongodb/ManagementBackupMongoDbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rizki.mufrizal.management.backup.mongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagementBackupMongoDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
