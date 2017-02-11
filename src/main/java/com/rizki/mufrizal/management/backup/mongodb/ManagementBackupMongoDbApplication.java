eda46d819d899473caa2ff694b4bda40f9b451b4
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-12 00:51:56 +0700

    update

--- src/main/java/com/rizki/mufrizal/management/backup/mongodb/ManagementBackupMongoDbApplication.java
+++ src/main/java/com/rizki/mufrizal/management/backup/mongodb/ManagementBackupMongoDbApplication.java
@@ -3,10 +3,21 @@ package com.rizki.mufrizal.management.backup.mongodb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since Feb 11, 2017
+ * @Time 8:40:05 PM
+ * @Encoding UTF-8
+ * @Project Management-Backup-MongoDB
+ * @Package com.rizki.mufrizal.management.backup.mongodb
+ *
+ */
 @SpringBootApplication
 public class ManagementBackupMongoDbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ManagementBackupMongoDbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementBackupMongoDbApplication.class, args);
+    }
 }
