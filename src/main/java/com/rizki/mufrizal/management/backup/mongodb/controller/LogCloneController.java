861fe2495369fc49db39a901801aa740fe748bc0
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-12 16:37:10 +0700

    update folder

--- src/main/java/com/rizki/mufrizal/management/backup/mongodb/controller/LogCloneController.java
+++ src/main/java/com/rizki/mufrizal/management/backup/mongodb/controller/LogCloneController.java
@@ -11,6 +11,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,11 +31,15 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @Package com.rizki.mufrizal.management.backup.mongodb.controller
  *
  */
+@PropertySource("classpath:application.yml")
 @Controller
 public class LogCloneController {
 
     @Autowired
     private LogCloneRepository logCloneRepository;
+
+    @Autowired
+    private Environment environment;
 
     @Secured(value = {"ROLE_USER", "ROLE_ADMINISTRATOR"})
     @RequestMapping(value = "/", method = RequestMethod.GET)
@@ -51,28 +57,28 @@ public class LogCloneController {
     @Secured(value = {"ROLE_ADMINISTRATOR"})
     @RequestMapping(value = "/backup", method = RequestMethod.GET)
     public String getBackup(Model model) {
-        model.addAttribute("backups", getFolders("/var/backups/mongobackups", ""));
+        model.addAttribute("backups", getFolders(environment.getRequiredProperty("folder.backup"), ""));
         return "BackupView";
     }
 
     @Secured(value = {"ROLE_ADMINISTRATOR"})
     @RequestMapping(value = "/backup-month", method = RequestMethod.GET)
     public String getBackupMonth(Model model, @RequestParam("month") String month) {
-        model.addAttribute("backups", getFolders("/var/backups/mongobackups", month));
+        model.addAttribute("backups", getFolders(environment.getRequiredProperty("folder.backup"), month));
         return "BackupMonthView";
     }
 
     @Secured(value = {"ROLE_ADMINISTRATOR"})
     @RequestMapping(value = "/backup-month-date", method = RequestMethod.GET)
     public String getBackupDate(Model model, @RequestParam("month") String month, @RequestParam("date") String date) {
-        model.addAttribute("backups", getFolders("/var/backups/mongobackups", month + "/" + date));
+        model.addAttribute("backups", getFolders(environment.getRequiredProperty("folder.backup"), month + "/" + date));
         return "BackupDateView";
     }
 
     @Secured(value = {"ROLE_ADMINISTRATOR"})
     @RequestMapping(value = "/backup-month-date-hours", method = RequestMethod.GET)
     public String getBackupHours(Model model, @RequestParam("month") String month, @RequestParam("date") String date, @RequestParam("hours") String hours) {
-        model.addAttribute("backups", getFiles("/var/backups/mongobackups", month + "/" + date + "/" + hours + "/" + "th-prod-2016"));
+        model.addAttribute("backups", getFiles(environment.getRequiredProperty("folder.backup"), month + "/" + date + "/" + hours + "/" + "th-prod-2016"));
         return "BackupHoursView";
     }
 
@@ -80,7 +86,7 @@ public class LogCloneController {
     @RequestMapping(value = "/backup-delete", method = RequestMethod.GET)
     public String deleteBackup(@RequestParam("month") String month) {
         try {
-            FileUtils.deleteDirectory(new File("/var/backups/mongobackups/" + month + "/"));
+            FileUtils.deleteDirectory(new File(environment.getRequiredProperty("folder.backup") + "/" + month + "/"));
         } catch (IOException ex) {
             Logger.getLogger(LogCloneController.class.getName()).log(Level.SEVERE, null, ex);
         }
